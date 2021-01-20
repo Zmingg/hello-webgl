25f3462bfe51e5e9b9d35ae8c37c6fe4654ac1fe
zhaiming <zhaiming@aecg.com.cn>
2021-01-20 17:13:53 +0800

    TRIANGLE_STRIP LINE

--- index.js
+++ index.js
@@ -1,89 +1,79 @@
-function createProgram(gl, vertexShader, fragmentShader) {
-  var program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
- 
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
-}
-
-function createShader(gl, type, source) {
-  var shader = gl.createShader(type); // 创建着色器对象
-  gl.shaderSource(shader, source); // 提供数据源
-  gl.compileShader(shader); // 编译 -> 生成着色器
-  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
- 
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-}
-
 // Get A WebGL context
 var canvas = document.getElementById("canvas");
-var gl = canvas.getContext('webgl');;
+var gl = canvas.getContext('webgl');
+// canvas.width = 300;
+// canvas.height = 200;
  
 // Get shaders 
 var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
 var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
  
-var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+var vertexShader = window.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+var fragmentShader = window.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-var program = createProgram(gl, vertexShader, fragmentShader);
+var program = window.createProgram(gl, vertexShader, fragmentShader);
 gl.useProgram(program);
- 
-// look up where the vertex data needs to go.
-var positionLocation = gl.getAttribLocation(program, "a_position");
+
+const path = [
+  10, 0,
+  50, 100,
+  100, -100,
+  140, 0,
+  150, 100,
+  160, 0,
+  300, 0,
+];
+const { current, prev, next } = window.duplicate(path);
+window.enableVertex(gl, "a_prev", prev, 2);
+window.enableVertex(gl, "a_next", next, 2);
+window.enableVertex(gl, "a_position", current, 2);
+window.enableVertex(gl, "a_corner", [
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+  -1, 0,
+  1, 0,
+], 2);
+console.log(current)
+
 
 // Create a buffer and put a single clipspace rectangle in
 // it (2 triangles)
-var buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array([
-    -1.0, 1.0,
-    -1.0, -1.0,
-    0, 1.0,
-    0, -1.0,
-    // 1.0, 1.0,
-    // 1.0, -1.0,
-  ]),
-  gl.STATIC_DRAW
-);
-gl.enableVertexAttribArray(positionLocation);
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+// var positionLocation = gl.getAttribLocation(program, "a_position");
+// var buffer = gl.createBuffer();
+// gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+// gl.bufferData(
+//   gl.ARRAY_BUFFER,
+//   new Float32Array([
+//     10, 0,
+//     20, 0,
+//     50, 50,
+//     60, 50,
+//     100, -50,
+//     110, -50,
+//     140, 0,
+//     150, 0
+//   ]),
+//   gl.STATIC_DRAW
+// );
+// gl.enableVertexAttribArray(positionLocation);
+// gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-const randomColorArray = (count) => {
-  const colorCount = count * 3;
-  return (new Array(colorCount)).fill().map(v => Math.random());
-}
-
-var colorLocation = gl.getAttribLocation(program, "a_color");
-console.log(randomColorArray(6))
-
-var colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array([
-    1.0, 1.0, 0,
-    1.0, 1.0, 0,
-    1.0, 1.0, 0,
-    0, 1.0, 0,
-  ]),
-  gl.STATIC_DRAW
-);
-gl.enableVertexAttribArray(colorLocation);
-gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
- 
 // draw
 // gl.drawArrays(gl.TRIANGLES, 0, 4); 
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4); 
+var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+gl.uniform3fv(colorUniformLocation, new Float32Array([0.0, 0.0, 0.0]));
+var lineWidthUniformLocation = gl.getUniformLocation(program, "u_lineWidth");
+gl.uniform1f(lineWidthUniformLocation, 4.0);
+gl.drawArrays(gl.TRIANGLE_STRIP, 0, path.length); 
---
+++ lib.js
@@ -0,0 +1,65 @@
+(function() {
+  window.duplicate = (positions) => {
+    const curRet = [];
+    const prevRet = [];
+    const nextRet = [];
+    positions.forEach((position, idx) => {
+      if (idx % 2 === 1) {
+        const prevX = idx - 2 < 0 ? positions[idx - 1] : positions[idx - 3];
+        const prevY = idx - 2 < 0 ? position : positions[idx - 2];
+        const curX = positions[idx - 1];
+        const curY = position;
+        const nextX = idx + 2 > positions.length ? positions[idx - 1] : positions[idx + 1];
+        const nextY = idx + 2 > positions.length ? position : positions[idx + 2];
+        curRet.push(curX, curY, curX, curY);
+        prevRet.push(prevX, prevY, prevX, prevY);
+        nextRet.push(nextX, nextY, nextX, nextY);
+      }
+    });
+    return {
+      current: curRet,
+      prev: prevRet,
+      next: nextRet
+    };
+  }
+
+  window.createProgram = (gl, vertexShader, fragmentShader) => {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
+   
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+  
+  window.createShader = (gl, type, source) => {
+    var shader = gl.createShader(type); // 创建着色器对象
+    gl.shaderSource(shader, source); // 提供数据源
+    gl.compileShader(shader); // 编译 -> 生成着色器
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
+   
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
+
+  window.enableVertex = (gl, locationName, array, size) => {
+    var location = gl.getAttribLocation(program, locationName);
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(array),
+      gl.STATIC_DRAW
+    );
+    gl.enableVertexAttribArray(location);
+    gl.vertexAttribPointer(location, size, gl.FLOAT, false, 0, 0);
+  }
+})();
